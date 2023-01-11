d6badfd7b0a3cefe3deda6823afbb2360a1e28dd
Nic Rummel <nicrummel@gmail.com>
2023-01-11 12:53:12 -0700

    add env.sh for setting up enviornment vars

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## Optain top level directory: PATH/TO/bridges_to_prosperity_ML
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+## Set python path
+export PYTHONPATH=$DIR:$PYTHONPATH

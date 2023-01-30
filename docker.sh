366e16fa11da3a358edad5b0a48d7311c88e8f19
Erick Verleye <erve3705@colorado.edu>
2023-01-30 13:34:04 -0700

    Updating docker.sh

--- docker.sh
+++ docker.sh
@@ -7,4 +7,4 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo chmod 666 /var/run/docker.sock
 docker system prune -f
-docker run --name hella --hostname localhost -v $SCRIPT_DIR:/b2p -it -p 8888:8888 earthlab/earth-analytics-python-env
+docker run --name hella --hostname localhost -v $SCRIPT_DIR:/b2p -it -p 8888:8888 earthlabcu/b2p

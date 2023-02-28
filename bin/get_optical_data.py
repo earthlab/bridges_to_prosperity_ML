4ecd789192ae9eadd23b46f03b6340b6182e7827
nic rummel <nicholas.rummel@colorado.edu>
2023-02-28 16:56:52 +0000

    update

--- bin/get_optical_data.py
+++ bin/get_optical_data.py
@@ -41,68 +41,68 @@ def upload_to_s3(filename, key):
             Key=key,
             Callback=lambda bytes_transferred: pbar.update(bytes_transferred),
         )
-
-## Start script
-region_info_file = 'data/region_info.yaml'
-with open(region_info_file, 'r') as file:
-    region_info = yaml.safe_load(file)
-api = SinergiseSentinelAPI()
-print('======================================================')
-print('Staring Scipt')
-for region, info in region_info.items():
-    dates = info['dates']
-    if region != "Cote d'Ivoire": 
-        continue
-    for district, district_info in info['districts'].items():
-        # if district_info['spec'] != 'train':
-        #     continue
-        bbox = district_info['bbox']
-        print(f'{region}/{district}\n' 
-            f'\tdates : {dates}\n'
-            f'\tbbox : {bbox}') 
-        # define src and dst dirs
-        s2_dir = os.path.join(BASE_DIR, 'data', 'sentinel2', region, district)
-        composite_dir = os.path.join(BASE_DIR, 'data', 'composites', region, district)
-        os.makedirs(s2_dir, exist_ok=True)
-        os.makedirs(composite_dir, exist_ok=True)
-        # download data from s2 api
-        print('--------------------------------')
-        print('downloading sentinel2')
-        for date in dates:
-            if district == 'Kasese':
-                continue
-            start_date, end_date = date
-            api.download(bbox, BUFFER, s2_dir, start_date, end_date)
-        # create composite
-        print('--------------------------------')
-        print('creating composites')
-        for coord in os.listdir(s2_dir):
-            print(f'Creating composite for {region}/{district}/{coord}...')
-            multiband_file_path = imaging.createComposite(
-                s2_dir, 
-                composite_dir, 
-                coord, 
-                BANDS, 
-                DTYPE,
-                SLICES,
-                N_CORES>1
-            )
+if __name__ == "__main__":
+    ## Start script
+    region_info_file = 'data/region_info.yaml'
+    with open(region_info_file, 'r') as file:
+        region_info = yaml.safe_load(file)
+    api = SinergiseSentinelAPI()
+    print('======================================================')
+    print('Staring Scipt')
+    for region, info in region_info.items():
+        dates = info['dates']
+        if region != "Cote d'Ivoire": 
+            continue
+        for district, district_info in info['districts'].items():
+            # if district_info['spec'] != 'train':
+            #     continue
+            bbox = district_info['bbox']
+            print(f'{region}/{district}\n' 
+                f'\tdates : {dates}\n'
+                f'\tbbox : {bbox}') 
+            # define src and dst dirs
+            s2_dir = os.path.join(BASE_DIR, 'data', 'sentinel2', region, district)
+            composite_dir = os.path.join(BASE_DIR, 'data', 'composites', region, district)
+            os.makedirs(s2_dir, exist_ok=True)
+            os.makedirs(composite_dir, exist_ok=True)
+            # download data from s2 api
+            print('--------------------------------')
+            print('downloading sentinel2')
+            for date in dates:
+                if district == 'Kasese':
+                    continue
+                start_date, end_date = date
+                api.download(bbox, BUFFER, s2_dir, start_date, end_date)
+            # create composite
+            print('--------------------------------')
+            print('creating composites')
+            for coord in os.listdir(s2_dir):
+                print(f'Creating composite for {region}/{district}/{coord}...')
+                multiband_file_path = imaging.createComposite(
+                    s2_dir, 
+                    composite_dir, 
+                    coord, 
+                    BANDS, 
+                    DTYPE,
+                    SLICES,
+                    N_CORES>1
+                )
+                upload_to_s3(
+                    multiband_file_path, 
+                    os.path.join('composites', region, district, os.path.basename(multiband_file_path))
+                )
+            # Compress sentinel2
+            print('-----------------------------')
+            print('Compressing raw s2')
+            tar_file =  os.path.join(BASE_DIR, 'data', 'sentinel2', f's2_{region}_{district}.tar.gz')
+            tar_cmd = f'tar -czvf {tar_file} --remove-files {s2_dir}'
+            process = subprocess.Popen(tar_cmd.split(), shell=False)
+            process.communicate()
+            # Upload raw s2 to s3
             upload_to_s3(
-                multiband_file_path, 
-                os.path.join('composites', region, district, os.path.basename(multiband_file_path))
+                tar_file, 
+                os.path.join('sentinel2_raw', os.path.basename(tar_file))
             )
-        # Compress sentinel2
-        print('-----------------------------')
-        print('Compressing raw s2')
-        tar_file =  os.path.join(BASE_DIR, 'data', 'sentinel2', f's2_{region}_{district}.tar.gz')
-        tar_cmd = f'tar -czvf {tar_file} --remove-files {s2_dir}'
-        process = subprocess.Popen(tar_cmd.split(), shell=False)
-        process.communicate()
-        # Upload raw s2 to s3
-        upload_to_s3(
-            tar_file, 
-            os.path.join('sentinel2_raw', os.path.basename(tar_file))
-        )
-        # remove raw data from this vm
-        os.remove(tar_file)
-        print('============================================\n\n')
+            # remove raw data from this vm
+            os.remove(tar_file)
+            print('============================================\n\n')

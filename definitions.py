b24a105617c4f5546e4e85d1a4cf12fe91ca2e2b
Erick Verleye <erve3705@colorado.edu>
2023-04-06 17:25:34 -0600

    definitions.py fix

--- definitions.py
+++ definitions.py
@@ -1,7 +1,7 @@
 import os
 
 B2P_DIR = os.path.abspath(os.path.dirname(__file__))
-REGION_FILE_PATH = os.path.join(B2P_DIR, 'data', 'region_info.yml')
+REGION_FILE_PATH = os.path.join(B2P_DIR, 'data', 'region_info.yaml')
 COMPOSITE_DIR = os.path.join(B2P_DIR, 'data', 'composites')
 TILE_DIR = os.path.join(B2P_DIR, 'data', 'tiles')
 TRUTH_DIR = os.path.join(B2P_DIR, 'data', 'ground_truth')

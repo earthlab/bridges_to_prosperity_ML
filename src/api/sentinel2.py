909f9b546928e370282748c3886892812641a32d
nic rummel <nicholas.rummel@colorado.edu>
2023-04-25 22:54:08 +0000

    add band 08 (near IR) to sentinel 2 api

--- src/api/sentinel2.py
+++ src/api/sentinel2.py
@@ -310,7 +310,7 @@ class SinergiseSentinelAPI:
                 )
                 if 'Contents' in list(response.keys()):
                     info += [(v['Key'], v['Size']) for v in response['Contents'] if
-                             'B02.jp2' in v['Key'] or 'B03.jp2' in v['Key'] or 'B04.jp2' in v['Key'] or
+                             'B02.jp2' in v['Key'] or 'B03.jp2' in v['Key'] or 'B04.jp2' in v['Key'] or 'B08.jp2' in v['Key'] or
                              'MSK_CLOUDS_B00.gml' in v['Key']]
 
         return info

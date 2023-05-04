1d520821443f77eb9792b5be692139bb0e168b51
Erick Verleye <erve3705@colorado.edu>
2023-05-04 11:28:05 -0600

    Bringing all data together for cube

--- bin/hack_hypercubes.py
+++ bin/hack_hypercubes.py
@@ -12,11 +12,12 @@ import rasterio
 import yaml
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
+from osgeo import gdal
 
 from bin.composites_to_tiles import create_tiles
 from definitions import REGION_FILE_PATH, COMPOSITE_DIR, TILE_DIR, TRUTH_DIR, SENTINEL_2_DIR, ELEVATION_DIR, SLOPE_DIR
 from src.utilities.aws import initialize_s3
-from src.utilities.imaging import create_composite
+from src.utilities.imaging import numpy_array_to_raster
 from src.utilities.config_reader import CONFIG
 from src.utilities.imaging import elevation_to_slope, subsample_geo_tiff
 from src.utilities.coords import get_bridge_locations
@@ -25,8 +26,22 @@ from bin.sentinel2_to_composite import sentinel2_to_composite
 from bin.prepare_optical_inputs import download_composites
 from src.api.sentinel2 import SinergiseSentinelAPI
 from src.api.lp_daac import Elevation
+from src.api.osm import getOsm
 
 CORES = mp.cpu_count() - 1
+
+
+def combine_bands(source_file: str, target_file: str):
+    with rasterio.open(source_file, 'r') as src_file:
+        # copy and update the metadata from the input raster for the output
+        meta = src_file.meta.copy()
+        d = meta['count']
+        meta.update(
+            count=d + 1
+        )
+        with rasterio.open(target_file, 'w+', **meta) as dst:
+            for i in range(d):
+                dst.write_band(i + 1, src_file.read(i + 1))
 
 
 def hack_hypercubes(requested_locations: List[str], composites_dir: str = COMPOSITE_DIR,
@@ -62,37 +77,37 @@ def hack_hypercubes(requested_locations: List[str], composites_dir: str = COMPOS
                 assert os.path.isfile(ir_comp), f'IR composite should already exist for {ir_comp}'
                 dst_tiff = rgb_comp.replace('multiband.tiff', 'RGBplusIR.tiff')
 
-                with rasterio.open(rgb_comp, 'r') as src_rgb:
-                    with rasterio.open(ir_comp, 'r') as src_ir:
-                        # copy and update the metadata from the input raster for the output
-                        meta = src_rgb.meta.copy()
-                        d = meta['count']  # should be 3 or 4 to include RGB and maybe IR (near wave)
-                        meta.update(
-                            count=d + 1
-                        )
-                        with rasterio.open(dst_tiff, 'w+', **meta) as dst:
-                            if debug:
-                                print('Copying RGB data to hypercube tiff')
-                            for i in range(d):
-                                dst.write_band(i + 1, src_rgb.read(i + 1))
-                            dst.write_band(d + 1, src_ir.read(0))
-                return
+                # return
                 # Get elevation data for requested region
                 # TODO: Make file naming classes
                 # TODO: Check if files already exist before making them
-                elevation_outfile = os.path.join(ELEVATION_DIR, region, district, base_name.replace('mulitband.tiff',
+                elevation_outfile = os.path.join(ELEVATION_DIR, region, district, base_name.replace('multiband.tiff',
                                                                                                     'elevation.tiff'))
                 elevation_api.download_district(elevation_outfile, region, district, buffer=100)
 
-                slope_outfile = os.path.join(SLOPE_DIR, region, district, base_name.replace('mulitband.tiff',
+                slope_outfile = os.path.join(SLOPE_DIR, region, district, base_name.replace('multiband.tiff',
                                                                                             'slope.tiff'))
                 elevation_to_slope(elevation_outfile, slope_outfile)
 
                 high_res_elevation = subsample_geo_tiff(elevation_outfile, dst_tiff)
                 high_res_slope = subsample_geo_tiff(slope_outfile, dst_tiff)
-                # getOsm(dst_tiff, osm_tiff)
-                # sync to s3
-                # then delete everything for this district on local memory
+
+                dst_tiff_file = gdal.Open(dst_tiff)
+                high_res_geo_reference = dst_tiff_file.GetGeoTransform()
+
+                numpy_array_to_raster(elevation_outfile, high_res_elevation, high_res_geo_reference, 'wgs84')
+                numpy_array_to_raster(slope_outfile, high_res_slope, high_res_geo_reference, 'wgs84')
+
+                osm_outfile = os.path.join(SLOPE_DIR, region, district, base_name.replace('multiband.tiff',
+                                                                                          'osm.tiff'))
+
+                getOsm(dst_tiff, osm_outfile)
+
+                combine_bands(rgb_comp, dst_tiff)
+                combine_bands(ir_comp, dst_tiff)
+                combine_bands(osm_outfile, dst_tiff)
+                combine_bands(elevation_outfile, dst_tiff)
+                combine_bands(slope_outfile, dst_tiff)
 
         else:
             assert False, "not implemented"

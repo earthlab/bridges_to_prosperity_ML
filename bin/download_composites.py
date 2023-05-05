aaa5532156e6b4ca43aeb132ea9879247ec5cebd
Erick Verleye <erve3705@colorado.edu>
2023-05-04 18:52:17 -0600

    Downloading the old way

--- bin/download_composites.py
+++ bin/download_composites.py
@@ -17,36 +17,33 @@ from definitions import REGION_FILE_PATH, COMPOSITE_DIR, TILE_DIR, TRUTH_DIR
 from src.utilities.aws import initialize_s3
 from src.utilities.config_reader import CONFIG
 from src.utilities.coords import get_bridge_locations
+from file_types import OpticalComposite
 
 CORES = mp.cpu_count() - 1
 
 
 def this_download(location_request_info: Tuple[str, int, int, str]) -> None:
     for location_path, composite_size, destination, position, bucket_name in location_request_info:
-        s3, client = initialize_s3(CONFIG.AWS.BUCKET)
-        print(s3, client)
+        s3, client = initialize_s3(bucket_name)
         if not client:
-            bucket = s3.Bucket(CONFIG.AWS.BUCKET)
+            bucket = s3.Bucket(bucket_name)
         else:
             bucket = s3
         if os.path.isfile(destination):
             return None
         dst_root = os.path.split(destination)[0]
         os.makedirs(dst_root, exist_ok=True)
-        print('Downloading')
         with tqdm(total=int(composite_size), unit='B', unit_scale=True, desc=location_path, leave=False,
                   position=int(position)) as pbar:
-            print(destination)
             bucket.download_file(Key=location_path, Filename=destination, Bucket=bucket_name,
                                  Callback=lambda bytes_transferred: pbar.update(bytes_transferred))
     return None
 
 
-def download_composites(region: str = None, districts: List[str] = None, composites_dir: str = COMPOSITE_DIR,
+def download_composites(region: str = None, districts: List[str] = None,
                         s3_bucket_name: str = CONFIG.AWS.BUCKET, bucket_composite_dir: str = 'composites',
                         cores: int = mp.cpu_count() - 1):
     s3, client = initialize_s3(s3_bucket_name)
-    print(s3, client)
     if not client:
         s3_bucket = s3.Bucket(s3_bucket_name)
     else:
@@ -73,8 +70,12 @@ def download_composites(region: str = None, districts: List[str] = None, composi
     for location in requested_locations:
         for obj in s3_bucket.objects.filter(Prefix=location):
             if obj.key.endswith('.tiff'):
-                destination = os.path.join(composites_dir, obj.key.strip(f'{bucket_composite_dir}/'))
+                destination = os.path.join(COMPOSITE_DIR, obj.key.strip(f'{bucket_composite_dir}/'))
                 location_info.append((obj.key, obj.size, destination))
+            # s3_composite = OpticalComposite.create(obj.key)
+            # if s3_composite:
+            #     destination = s3_composite.archive_path
+            #     location_info.append((obj.key, obj.size, destination))
 
     parallel_inputs = []
     for i, location in enumerate(np.array_split(location_info, cores)):
@@ -106,8 +107,6 @@ if __name__ == "__main__":
         required=False,
         help='Name of the composite district (Ex. Fafan Ibanda)'
     )
-    parser.add_argument('--composites_dir', '-c', required=False, default=COMPOSITE_DIR, type=str,
-                        help=f'Directory where composites will be written to. Default is {COMPOSITE_DIR}')
     parser.add_argument('--s3_bucket_name', '-b', required=False, default=CONFIG.AWS.BUCKET, type=str,
                         help='Name of s3 bucket to search for composites in. Default is from project config, which is'
                              f' currently set to {CONFIG.AWS.BUCKET}')
@@ -117,6 +116,6 @@ if __name__ == "__main__":
                         help='Number of cores to use when making tiles in parallel. Default is cpu_count - 1')
     args = parser.parse_args()
 
-    download_composites(region=args.region, districts=args.districts, composites_dir=args.composites_dir,
+    download_composites(region=args.region, districts=args.districts,
                         s3_bucket_name=args.s3_bucket_name, bucket_composite_dir=args.bucket_composite_dir,
                         cores=args.cores)

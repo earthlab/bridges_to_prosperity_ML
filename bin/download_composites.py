1fe4551b51e8cc878c02cb099e78df52043e1dff
Erick Verleye <erve3705@colorado.edu>
2023-05-04 18:05:49 -0600

    Fixing download_composites.py

--- bin/download_composites.py
+++ bin/download_composites.py
@@ -24,6 +24,7 @@ CORES = mp.cpu_count() - 1
 def this_download(location_request_info: Tuple[str, int, int, str]) -> None:
     for location_path, composite_size, destination, position in location_request_info:
         s3, client = initialize_s3(CONFIG.AWS.BUCKET)
+        print(s3, client)
         if not client:
             bucket = s3.Bucket(CONFIG.AWS.BUCKET)
         else:

0bdaae4644678dd1b088a1be084db07043a817ad
Erick Verleye <erve3705@colorado.edu>
2023-05-09 00:46:04 -0600

    Adding refresh token function

--- src/utilities/aws.py
+++ src/utilities/aws.py
@@ -6,6 +6,8 @@ import tqdm
 from src.utilities.config_reader import CONFIG
 from definitions import COMPOSITE_DIR
 from file_types import OpticalComposite
+from src.api.sentinel2 import APIAuth
+import boto3
 
 
 def upload_to_s3(session, filename: str, key: str, bucket: str = None):
@@ -26,3 +28,24 @@ def upload_to_s3(session, filename: str, key: str, bucket: str = None):
             Key=key,
             Callback=lambda bytes_transferred: pbar.update(bytes_transferred),
         )
+
+
+def refresh_token():
+    s = APIAuth('')
+    s.parse_aws_credentials()
+
+    session = boto3.Session(
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        aws_session_token=os.environ['AWS_SESSION_TOKEN'],
+        region_name='us-west2'
+    )
+
+    # Check the current expiration time of the session token
+    print(session.get_credentials().expiry_time)
+
+    # Refresh the session token
+    session.get_credentials().refresh()
+
+    # Check the new expiration time of the session token
+    print(session.get_credentials().expiry_time)

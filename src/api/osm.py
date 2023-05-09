9249116df4612cfe91f4737d3393996a388a3888
Erick Verleye <erve3705@colorado.edu>
2023-05-09 00:36:31 -0600

    Using old bbox

--- src/api/osm.py
+++ src/api/osm.py
@@ -22,12 +22,8 @@ def getOsm(s2_tiff: str, dst_tiff: str, debug: bool = False):
     assert os.path.isfile(s2_tiff), f'{s2_tiff} DNE'
     # the bounding box that shapely uses is a set of 4 (x,y) pairs, ox wants xmin, xmax, ymin, ymax
     (tl,tr,br,bl) = tiff_to_bbox(s2_tiff)
-    print((tl,tr,br,bl))
     mgrs_bbox = mgrs_to_bbox(OpticalComposite.create(s2_tiff).mgrs)
     bbox = [tl[0], br[0], tl[1], br[1]]
-    bbox = [mgrs_bbox[3], mgrs_bbox[0], mgrs_bbox[1], mgrs_bbox[2]]
-    print(bbox)
-    print('bbox', bbox)
     # Call to ox api to get geometries for specific tags
     if debug: print('Getting water from osm')
     water = ox.geometries.geometries_from_bbox(*bbox, TAGS_WATER)
@@ -38,7 +34,6 @@ def getOsm(s2_tiff: str, dst_tiff: str, debug: bool = False):
     boundary_trim = boundary.loc[:,['geometry', 'boundary']].dropna()
     # convert to crs that matches the sentinel2
     epsg_code = get_utm_epsg(tl[0], tl[1])
-    print(mgrs_to_bbox(OpticalComposite.create(s2_tiff).mgrs))
     water_trim = water_trim.to_crs(f'epsg:{epsg_code}')
     boundary_trim = boundary_trim.to_crs(f'epsg:{epsg_code}')
     # Turn this into an iterable that will be used later by features 

c83eac3604a15201070d82ca4f61be9cfff9f3d8
Erick Verleye <erve3705@colorado.edu>
2023-05-11 01:03:52 -0600

    Adding mgrs

--- bin/create_data_cubes.py
+++ bin/create_data_cubes.py
@@ -199,7 +199,8 @@ def create_date_cubes(s3_bucket_name: str = CONFIG.AWS.BUCKET, cores: int = CORE
 
         # Download any s2 data that doesn't exist
         # TODO: Find a way to not look for overlapping tiles if files already exist
-        # s2_dir = os.path.join(SENTINEL_2_DIR, region, district)
+        s2_dir = os.path.join(SENTINEL_2_DIR, region, district)
+        os.makedirs(s2_dir, exist_ok=True)
         # for date in dates:
         #     sentinel2_api.download(bbox, 100, s2_dir, date[0], date[1], bands=['B08'])
 

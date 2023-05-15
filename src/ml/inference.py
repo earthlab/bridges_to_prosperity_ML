0b0161139248874017e250868714a486b6526f2d
Erick Verleye <erve3705@colorado.edu>
2023-05-14 19:06:55 -0600

    Debugging

--- src/ml/inference.py
+++ src/ml/inference.py
@@ -91,6 +91,7 @@ def inference_torch(model_file_path: str, tile_csv_path: str, results_csv_path: 
             )
             res_df.loc[ix, 'tile'] = tile
             res_df.loc[ix, 'bbox'] = bbox
+            print(target)
             if target:
                 res_df.loc[ix, 'target'] = target
             res_df.loc[ix, 'pred'] = pred.cpu().numpy()
